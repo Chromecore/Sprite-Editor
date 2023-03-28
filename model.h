172a7c5ad2805c0f0467c7bc93d331e747cb6702
jackk <jackk@LAPTOP-VKSJT2LL>
2023-03-28 13:42:27 -0600

    filled out model header

--- model.h
+++ model.h
@@ -1,11 +1,35 @@
 #ifndef MODEL_H
 #define MODEL_H
-
+#include <QImage>
+using std::vector;
 
 class Model
 {
+private:
+    vector<QImage> images; //images that make up sprite
+
+    QColor currentColor; // current drawing color
+    int fps;          // how fast sprite moves in preview
+    bool onionSkin; //form of shadow to look at previos portions of sprite
+    int spriteSize; //size of canvas
+    int currentImageIndex = 0; // current index of the image in canvas
 public:
     Model();
+
+    void setImage(QImage image);
+    QImage getImage();
+    vector<QImage> getImages();
+    void setColor(QColor color);
+    QColor getColor();
+    void setFPS(int fps);
+    int getFPS();
+    bool isOnionSkin();
+    void isOnionSkin(bool is);
+    int getSpriteSize();
+    void setSpriteSize(int size);
+    void setCurrentIndex(int i);
+    void saveFile();
+
 };
 
 #endif // MODEL_H
