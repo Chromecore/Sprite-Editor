525e5ca8cd05706bb5be09b8a9890ea1c062ecc0
JonMercado <jonmercado15@yahoo.com>
2023-04-04 21:46:10 -0600

    Resolving error from last merge

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,11 +16,6 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    // setup saving and loading through the menubar
-    QAction *saveAction = ui->menubar->addAction("Save");
-    QAction *loadAction = ui->menubar->addAction("Load");
-    connect(saveAction, &QAction::triggered, Model::instance, &Model::saveFile);
-    connect(loadAction, &QAction::triggered, Model::instance, &Model::loadFile);
     connect(Model::instance,
             &Model::invalidFile,
             this,
